2173185dabcac132ce5859874e57bf51333595c6
adamcee <adam.cahan@gmail.com>
2021-07-26 13:07:43 -0500

    Node.js http server hello world with a small Content-Type exercise!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { describeTheWeatherToday } from './weather-app/index';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+ 
+const PORT = 9999;
+ 
+// TODO: What do you have to change to make our response be interpreted by the browser as HTML?
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('<h1>Hello, world!</h1><p>What a great day today!</p>');
+  response.end();
+});
+ 
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
 
-const isSunny = true;
-// TODO: Import forecast array and functions needed to describe the weather, use that real data.
-console.log(describeTheWeatherToday(isSunny, 7, 4));
+server.on('error', (error: NodeJS.ErrnoException) => {
+  if (error.code != 'FOO') {
+    throw error;
+  }
+})
